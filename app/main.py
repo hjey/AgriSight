0a37a6789612748a929c547e5d1a8dbe07083610
hjey <hyejeong.noh@gmail.com>
2025-04-01 00:02:39 +0900

    no message

---
+++ app/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from routes import router
+
+app = FastAPI()
+app.include_router(router)
+
+app.mount("/videos", StaticFiles(directory="/data"), name="videos")
+app.mount("/static", StaticFiles(directory="/app/static"), name="static")  # CSS, JS, 이미지 등
